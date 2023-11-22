98232c8111d0f4e5e04b46011de75ff05ccddc57
davidpchi <davidpchi@gmail.com>
2023-11-21 22:55:22 -0500

    Add GameService logic and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import { Flex, Text } from "@radix-ui/themes";
+import { GamesService } from "./services/GamesService";
+import { useState } from "react";
+import { Game } from "./types/Game";
 
 function App() {
+    const [data, setData] = useState<Game[] | undefined>();
+
+    if (data === undefined) {
+        GamesService.fetchGamesAsync().then((res) => {
+            setData(res);
+        });
+    }
+
+    console.log(data);
+
     return (
         <Flex>
             <Text>TEST</Text>
