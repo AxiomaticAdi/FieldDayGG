109e9003bf8271924c693c4d62b89ee2f18510b9
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-11-22 00:41:31 -0500

    React router added

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App.tsx";
 import "./index.css";
 import "@radix-ui/themes/styles.css";
 
 import { Theme } from "@radix-ui/themes";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <Theme>
-            <App />
+            <RouterProvider router={router} />
         </Theme>
     </React.StrictMode>,
 );
