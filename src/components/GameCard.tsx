bd28e922739543b0e1bf96763c4947d0aed270c3
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-11-24 16:11:42 -0500

    Remove images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,18 +1,11 @@
-import { Card, Flex, Inset, Text, Strong } from "@radix-ui/themes";
+import { Card, Flex, Text, Strong } from "@radix-ui/themes";
 import { Game } from "../types/Game";
 
-const placeholder =
-    "https://img.freepik.com/premium-vector/lightning-electric-power-vector-logo-design-element-energy-thunder-electricity-symbol_666746-43.jpg?size=338&ext=jpg&ga=GA1.1.1880011253.1699401600&semt=ais";
-
 export default function GameCardModal({ game }: { game: Game }) {
-    const imageUri = game.imgUri ?? placeholder;
-
-    console.log(game);
-
     return (
         <Flex className="p-5">
             <Card style={{ width: 200 }}>
-                <Inset clip="padding-box" side="top" pb="current">
+                {/* <Inset clip="padding-box" side="top" pb="current">
                     <img
                         src={imageUri}
                         alt={game.name}
@@ -24,7 +17,7 @@ export default function GameCardModal({ game }: { game: Game }) {
                             backgroundColor: "var(--gray-5)",
                         }}
                     />
-                </Inset>
+                </Inset> */}
                 <Text as="p" size="3" className="text-center">
                     <Strong>{game.name}</Strong>
                 </Text>
